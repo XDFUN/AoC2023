781b575f4596b3f039d8b9e3eb1d4d61b0570486
XDFUN <blechhosenheinz@gmail.com>
2023-12-06 18:25:10 +0100

    [EDIT] Day 4 Part 1, somehow working for example, but not my input

---
+++ AdventOfCode2023/Day4/Day4.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode2023.Day4;
+
+public static class Day4
+{
+    private static string[] ReadAllLines(string path)
+    {
+        return File.ReadAllLines(path);
+    }
+
+    private static string SumCardScores(this string[] input)
+    {
+        long score = (from card in input
+                     select card.Split(':', StringSplitOptions.TrimEntries)[1]
+                                .Split('|', StringSplitOptions.TrimEntries)
+                     into values
+                     let winningNumbers = values[0].Split(' ').ToHashSet()
+                     select values[1]
+                            .Split(' ')
+                            .Where(winningNumbers.Contains)
+                            .Aggregate(0L, (current, number) => current == 0 ? 1 : current * 2)).Sum();
+
+        return score.ToString();
+    }
+
+    public class Challenge1 : IChallenge
+    {
+        public string Complete()
+        {
+            return ReadAllLines(Path.GetFullPath(@".\Day4\input.txt")).SumCardScores();
+        }
+    }
+
+    public class Challenge2 : IChallenge
+    {
+        public string Complete()
+        {
+            return "";
+        }
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,18 +1,25 @@
 ﻿using AdventOfCode2023.Day1;
 using AdventOfCode2023.Day2;
 using AdventOfCode2023.Day3;
+using AdventOfCode2023.Day4;
 
-WriteDay(1);
+int days = 1;
+
+WriteDay(days++);
 WriteChallenge(1, new Day1.Challenge1().Complete());
 WriteChallenge(2, new Day1.Challenge2().Complete());
 
-WriteDay(2);
+WriteDay(days++);
 WriteChallenge(1, new Day2.Challenge1().Complete());
 WriteChallenge(2, new Day2.Challenge2().Complete());
 
-WriteDay(3);
+WriteDay(days++);
 WriteChallenge(1, new Day3.Challenge1().Complete());
 WriteChallenge(2, new Day3.Challenge2().Complete());
+
+WriteDay(days++);
+WriteChallenge(1, new Day4.Challenge1().Complete());
+WriteChallenge(2, new Day4.Challenge2().Complete());
 
 return;
 
